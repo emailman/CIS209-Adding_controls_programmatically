338aaf56b48891d1b3da7297b8e5831a1a4cf128
Eric Mailman <emailman@dtcc.edu>
2018-09-16 20:40:09 -0400

    First version

--- CIS209-Adding_controls_programmatically/Form1.cs
+++ CIS209-Adding_controls_programmatically/Form1.cs
@@ -12,9 +12,30 @@ namespace CIS209_Adding_controls_programmatically
 {
     public partial class Form1 : Form
     {
+        // My Controls
+        private Label lbl1 = new Label();
+        private TextBox tbx1 = new TextBox();
+        private Button btn1 = new Button();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lbl1.Text = "My label";
+            lbl1.Location = new Point(10, 25);
+
+            tbx1.Location = new Point(150, 25);
+
+            btn1.Text = "My Button";
+            btn1.Location = new Point(10, 50);
+
+            // Add controls
+            Controls.Add(lbl1);
+            Controls.Add(tbx1);
+            Controls.Add(btn1);
         }
     }
 }
